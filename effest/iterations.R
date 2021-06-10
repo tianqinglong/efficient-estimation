8f544c923c83b17006a083137df0d33379d31302
tianqinglong <qltian@iastate.edu>
2021-06-09 23:47:23 -0500

    add the blank function for iteration

---
+++ effest/iterations.R
@@ -0,0 +1,10 @@
+#-------------------------------
+# This function is to do the EM interations
+# Author: Qinglong Tian
+# Date: June 9, 2021
+#-------------------------------
+
+interateEM <- function()
+{
+  
+}
