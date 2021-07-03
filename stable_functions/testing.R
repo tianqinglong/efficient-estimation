0b614ae2feec5641b8deae6a91ca8d855b2ccadf
tianqinglong <qltian@iastate.edu>
2021-07-03 17:21:56 -0500

    unimportant

--- stable_functions/testing.R
+++ stable_functions/testing.R
@@ -8,26 +8,26 @@ source("estep.R")
 source("mstep.R")
 source("loglikelihood.R")
 
-n <- 500
+n <- 300
 coef1 <- c(1,1)
 sd <- 1
 ghn <- 8
-bn <- 3
-q <- 2
-max_iter <- 300
+bn <- 2
+q <- 3
+max_iter <- 500
 tol <- 1e-5
 
 
-X <- matrix(truncnorm::rtruncnorm(n, b = 0.7), ncol = 1)
+X <- matrix(truncnorm::rtruncnorm(n), ncol = 1)
 Y <- simuY(cbind(1, X), coef1, sd)
 Z <- X
 U <- NULL # \pi(Y)
 nsieves <- bn+q
 
 yy <- log(Y/(1-Y))
-YU <- cbind(1, yy)
-coef2 <- c(-1, 1)
-Obs <- simuMiss(YU, coef2)
+YU <- cbind(1, yy, yy^2)
+coef2 <- c(-.4, -2, 1)
+Obs <- simuMiss(YU, coef2, use_logit = F)
 
 dat <- cbind(Y, Obs, Z, U)
 colnames(dat) <- c("Y", "Obs", "X1")
@@ -42,7 +42,6 @@ hn <- min(sqrt(diag(vcov(lmMAR))))
 
 df_MNAR <- list(data = dat, Z_indices = 3, U_indices = NULL)
 emEstimate <- main(df_MNAR, coef1+0.5, sd+0.5, runif(bn+q, min = -1, max = 1), bn, q, ghn, max_iter, tol)
-emEstimate
 
 # Compute covariance matrix
 beta_mle <- emEstimate$Beta
@@ -51,6 +50,7 @@ tau_mle <- emEstimate$Tau
 
 temp <- ProfileCov(df_MNAR, min(hn, 1/sqrt(n)), beta_mle, sd_mle, tau_mle, bn, q, ghn, nsieves, 1, max_iter, tol)
 temp
+emEstimate
 
 # Tune the value of hn
 hnVec <- c(0.05*hn ,0.1*hn, 0.5*hn, hn, 1/sqrt(n), 0.025, 2*hn, 5*hn)
@@ -63,19 +63,9 @@ for(i in 1:length(hnVec))
 outList
 
 # Analysis of coverage
-rout <- readRDS("C:/Users/qltia/Desktop/rout.rds")
-rout[sapply(rout, is.character)] <- NULL
+rout2 <- readRDS("no_git/rout_tang2.rds")
+analysis(rout2, c(coef1, sd))
 
-sdVec <- t(sapply(rout, function(x) {sqrt(diag(x$Var))}))
-coefVec <- t(sapply(rout, function(x) {c(x$EM$Beta, x$EM$Sigma)}))
-lowerConf <- coefVec-1.96*sdVec
-upperConf <- coefVec+1.96*sdVec
+rout3 <- readRDS("no_git/rout_tang3.rds")
+analysis(rout3, c(coef1, sd))
 
-trueVal <- matrix(rep(c(1, 1, 1), times = length(rout)), byrow = T, ncol = 3)
-colMeans((trueVal < upperConf) & (trueVal > lowerConf))
-
-# Analysis of failures
-rout <- readRDS("C:/Users/qltia/Desktop/rout.rds")
-rout[sapply(rout, is.list)] <- NULL
-routf <- sapply(rout, function(x) {x})
-table(routf)
