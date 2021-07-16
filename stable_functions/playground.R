ff51c8db0757b31c653eb73c39b08ab98a111549
tianqinglong <qltian@iastate.edu>
2021-07-16 10:40:44 -0500

    finish pseudo likelihood method

--- stable_functions/playground.R
+++ stable_functions/playground.R
@@ -9,6 +9,8 @@ source("loglikelihood.R")
 source("analysis.R")
 ####################################
 
+## Setting 1
+
 n <- 1000
 coef1 <- c(1,1)
 sd <- 1
@@ -35,11 +37,127 @@ dat <- cbind(Y, Obs, Z, U)
 colnames(dat) <- c("Y", "Obs", "X1")
 dat %>% as.data.frame %>% mutate(OBS = as.factor(Obs), yy = log(Y/(1-Y))) %>% 
   ggplot(aes(x = X))+geom_density(aes(fill = OBS), alpha = 0.5)
+
 yObs <- Y[which(Obs == 1)]
 xObs <- X[which(Obs == 1),]
 yLM <- log(yObs/(1-yObs))
 lmMAR <- lm(yLM~xObs)
 hn <- min(sqrt(diag(vcov(lmMAR))))
+
 df_MNAR <- list(data = dat, Z_indices = 3, U_indices = NULL)
 
+opt1 <- optim(c(0.5, 0.5, 0.5), pseudo_loglikelihood_tian1, df_MNAR = df_MNAR, ghxw = gaussHermiteData(8), hessian = T)
 
+(emEstimate <- main(df_MNAR, coef1+0.5, sd+0.5, runif(bn+q, min = -1, max = 1), bn, q, ghn, max_iter, tol))
+# Compute covariance matrix
+beta_mle <- emEstimate$Beta
+sd_mle <- emEstimate$Sigma
+tau_mle <- emEstimate$Tau
+
+temp <- ProfileCov(df_MNAR, min(hn, 1/sqrt(n)), beta_mle, sd_mle, tau_mle, bn, q, ghn, nsieves, 1, max_iter, tol)
+temp
+cbind(beta_mle-qnorm(0.975)*sqrt(diag(temp))[1:length(beta_mle)],
+      beta_mle+qnorm(0.975)*sqrt(diag(temp))[1:length(beta_mle)])
+
+opt1$par-qnorm(0.975)*sqrt(diag(solve(opt1$hessian)))
+opt1$par+qnorm(0.975)*sqrt(diag(solve(opt1$hessian)))
+
+## Setting 2
+
+n <- 1000
+coef1 <- c(-1, 4, -2)
+sd <- 1
+
+Z <- matrix(rnorm(n), ncol = 1)
+U <- matrix(rnorm(n, 1-Z, 1), ncol = 1)
+X <- cbind(Z, U)
+Y <- simuY(cbind(1, X), coef1, sd)
+
+n_covarites <- ncol(X)
+yy <- log(Y/(1-Y))
+YU <- cbind(1, yy, U)
+coef2 <- c(4, -3, 1)
+Obs <- simuMiss(YU, coef2, use_logit = T)
+table(Obs)
+
+dat <- cbind(Y, Obs, Z, U)
+colnames(dat) <- c("Y", "Obs", paste("X", 1:(ncol(dat)-2), sep = ""))
+dat %>% as.data.frame %>% mutate(OBS = as.factor(Obs), yy = log(Y/(1-Y))) %>% 
+  ggplot(aes(x = U))+geom_density(aes(fill = OBS), alpha = 0.5)
+df_MNAR <- list(data = dat, Z_indices = 3, U_indices = 4)
+
+opt2 <- optim(c(-0.5, 3, -1, 0.5), pseudo_loglikelihood_tian2, df_MNAR = df_MNAR, ghxw = gaussHermiteData(8), hessian = T)
+
+yObs <- Y[which(Obs == 1)]
+xObs <- X[which(Obs == 1),]
+yLM <- log(yObs/(1-yObs))
+lmMAR <- lm(yLM~xObs)
+hn <- min(sqrt(diag(vcov(lmMAR))))
+
+ghn <- 6
+bn <- 3
+q <- 2
+max_iter <- 500
+tol <- 1e-4
+
+# Additive model
+nsieves_add <- (bn+q-1)*(ncol(U)+1)+1
+emEstimate <- main_additive(df_MNAR, coef1+0.1, sd+0.1, rep(0, times = nsieves_add), bn, q, ghn, max_iter, tol)
+emEstimate
+
+beta_mle <- emEstimate$Beta
+sd_mle <- emEstimate$Sigma
+tau_mle <- emEstimate$Tau
+
+temp <- ProfileCov_additive(df_MNAR, min(hn, 1/sqrt(n))*2, beta_mle, sd_mle, tau_mle, bn, q, ghn, nsieves_add, n_covarites, max_iter, tol)
+cbind(beta_mle-qnorm(0.975)*sqrt(diag(temp))[1:length(beta_mle)],
+      beta_mle+qnorm(0.975)*sqrt(diag(temp))[1:length(beta_mle)])
+
+opt2$par-qnorm(0.975)*sqrt(diag(solve(opt2$hessian)))
+opt2$par+qnorm(0.975)*sqrt(diag(solve(opt2$hessian)))
+
+## Setting 4
+
+n <- 1000
+coef1 <- c(-1, 4, -1, -1)
+sd <- 1
+
+ghn <- 8
+bn <- 4
+q <- 2
+max_iter <- 500
+tol <- 1e-5
+
+Z <- matrix(rnorm(n), ncol = 1)
+U <- matrix(rnorm(2*n, 1-Z, 1), ncol = 2)
+X <- cbind(Z, U)
+Y <- simuY(cbind(1, X), coef1, sd)
+
+n_covarites <- ncol(X)
+yy <- log(Y/(1-Y))
+YU <- cbind(1, yy, U)
+coef2 <- c(4, -3, 1, 1)
+Obs <- simuMiss(YU, coef2, use_logit =T)
+table(Obs)
+
+dat <- cbind(Y, Obs, Z, U)
+colnames(dat) <- c("Y", "Obs", paste("X", 1:(ncol(dat)-2), sep = ""))
+dat %>% as.data.frame %>% mutate(OBS = as.factor(Obs), yy = log(Y/(1-Y))) %>% 
+  ggplot(aes(x = yy))+geom_density(aes(fill = OBS), alpha = 0.5)
+df_MNAR <- list(data = dat, Z_indices = 3, U_indices = c(4, 5))
+
+opt4 <- optim(c(-0.5, 3, -0.5, -0.5, 0.5), pseudo_loglikelihood_tian4, df_MNAR = df_MNAR, ghxw = gaussHermiteData(8), hessian = T)
+opt4$par-qnorm(0.975)*sqrt(diag(solve(opt4$hessian)))
+opt4$par+qnorm(0.975)*sqrt(diag(solve(opt4$hessian)))
+
+nsieves_add <- (bn+q-1)*(ncol(U)+1)+1
+emEstimate <- main_additive(df_MNAR, coef1+0.1, sd+0.1, rep(0, nsieves_add), bn, q, ghn, max_iter, tol)
+emEstimate
+
+beta_mle <- emEstimate$Beta
+sd_mle <- emEstimate$Sigma
+tau_mle <- emEstimate$Tau
+
+temp1 <- ProfileCov_additive(df_MNAR, min(hn, 1/sqrt(n)), beta_mle, sd_mle, tau_mle, bn, q, ghn, nsieves_add, ncovariate, max_iter, tol)
+cbind(beta_mle-qnorm(0.975)*sqrt(diag(temp1))[1:length(beta_mle)],
+      beta_mle+qnorm(0.975)*sqrt(diag(temp1))[1:length(beta_mle)])
--- stable_functions/pseudo_likelihood.R
+++ stable_functions/pseudo_likelihood.R
@@ -1,3 +1,158 @@
 #-----------------------
-
+# Functions to implement the pseudolikelihood method
+# Author: Qinglong Tian
+# Date: July 15, 2021
 #-----------------------
+
+## First consider the situation that Z is independent of U
+organize_data <- function(df_MNAR)
+{
+  zIndex <- df_MNAR$Z_indices
+  uIndex <- df_MNAR$U_indices
+  
+  dat <- df_MNAR$data
+  dat_non_missing <- dat[dat[,"Obs"] == 1,]
+  
+  # Outputs
+  yVec <- dat_non_missing[,"Y"]
+  yVec <- log(yVec/(1-yVec))
+  
+  if (is.null(zIndex))
+  {
+    zMat <- NULL
+  }
+  else
+  {
+    zMat <- dat_non_missing[,zIndex]
+  }
+  
+  if (is.null(uIndex))
+  {
+    uMat <- NULL
+  }
+  else
+  {
+    uMat <- dat_non_missing[,uIndex]
+  }
+  
+  return(list(yVec = yVec,
+              zMat = zMat,
+              uMat = uMat))
+}
+
+### normal assumption: setting 1
+pseudo_loglikelihood_tian1 <- function(theta, df_MNAR, ghxw)
+{
+  # parameters
+  beta <- theta[-length(theta)]
+  sigma <- theta[length(theta)]
+  
+  # extract data
+  yzu <- organize_data(df_MNAR)
+  yVec <- yzu$yVec
+  XObs <- cbind(yzu$zMat, yzu$uMat)
+  
+  data <- df_MNAR$data
+  X <- data[,3]
+  
+  # Fit X - compute log-likelihood part2
+  f0 <- lm(X~1)
+  mu_x <- f0$coefficients
+  sigma_x <- sigma(f0)
+  
+  xRaw <- ghxw$x
+  wVec <- ghxw$w
+  
+  xReal <- matrix(sqrt(2)*xRaw*sigma_x+mu_x, ncol = 1)
+  muReal <- cbind(1, xReal) %*% matrix(beta, ncol = 1)
+  
+  log_int <- numeric(length(yVec))
+  for (i in 1:length(yVec)) {
+    log_int[i] <- log(sum(dnorm(yVec[i], mean = muReal, sd = sigma)*wVec)/sqrt(pi))
+  }
+  loglik2 <- -sum(log_int)
+  
+  # Compute log-likelihood part1
+  muRealObs <- cbind(1, XObs) %*% matrix(beta, ncol = 1)
+  loglik1 <- sum(dnorm(yVec, muRealObs, sigma, log = T))
+  
+  return(-(loglik1+loglik2))
+}
+
+### normal assumption: setting 2
+pseudo_loglikelihood_tian2 <- function(theta, df_MNAR, ghxw)
+{
+  # parameters
+  beta <- theta[-length(theta)]
+  sigma <- theta[length(theta)]
+  
+  # extract data
+  yzu <- organize_data(df_MNAR)
+  yVec <- yzu$yVec
+  XObs <- cbind(yzu$zMat, yzu$uMat)
+  
+  data <- df_MNAR$data
+  X <- data[,c(3,4)]
+  
+  # Fit X - compute log-likelihood part2
+  f0 <- lm(X[,1]~X[,2])
+  zCoef <- f0$coefficients
+  z_sigma <- sigma(f0)
+  
+  xRaw <- ghxw$x
+  wVec <- ghxw$w
+  
+  log_int <- numeric(length(yVec))
+  z_real_mu <- cbind(1, XObs[,2]) %*% matrix(zCoef, ncol=1)
+  for (i in 1:length(yVec)) {
+    xReal <- matrix(sqrt(2)*xRaw*z_sigma+z_real_mu[i], ncol = 1)
+    muReal <- cbind(1, xReal, rep(XObs[i,2], length(xReal))) %*% matrix(beta, ncol = 1)
+    log_int[i] <- log(sum(dnorm(yVec[i], mean = muReal, sd = sigma)*wVec)/sqrt(pi))
+  }
+  loglik2 <- -sum(log_int)
+  
+  # Compute log-likelihood part1
+  muRealObs <- cbind(1, XObs) %*% matrix(beta, ncol = 1)
+  loglik1 <- sum(dnorm(yVec, muRealObs, sigma, log = T))
+  
+  return(-(loglik1+loglik2))
+}
+
+### normal assumption: setting 4
+pseudo_loglikelihood_tian4 <- function(theta, df_MNAR, ghxw)
+{
+  # parameters
+  beta <- theta[-length(theta)]
+  sigma <- theta[length(theta)]
+  
+  # extract data
+  yzu <- organize_data(df_MNAR)
+  yVec <- yzu$yVec
+  XObs <- cbind(yzu$zMat, yzu$uMat)
+  
+  data <- df_MNAR$data
+  X <- data[,c(3,4,5)]
+  
+  # Fit X - compute log-likelihood part2
+  f0 <- lm(X[,1]~X[,2]+X[,3])
+  zCoef <- f0$coefficients
+  z_sigma <- sigma(f0)
+  
+  xRaw <- ghxw$x
+  wVec <- ghxw$w
+  
+  log_int <- numeric(length(yVec))
+  z_real_mu <- cbind(1, XObs[,c(2,3)]) %*% matrix(zCoef, ncol=1)
+  for (i in 1:length(yVec)) {
+    xReal <- matrix(sqrt(2)*xRaw*z_sigma+z_real_mu[i], ncol = 1)
+    muReal <- cbind(1, xReal, matrix(rep(XObs[i,c(2,3)], length(xReal)), ncol = 2, byrow = T)) %*% matrix(beta, ncol = 1)
+    log_int[i] <- log(sum(dnorm(yVec[i], mean = muReal, sd = sigma)*wVec)/sqrt(pi))
+  }
+  loglik2 <- -sum(log_int)
+  
+  # Compute log-likelihood part1
+  muRealObs <- cbind(1, XObs) %*% matrix(beta, ncol = 1)
+  loglik1 <- sum(dnorm(yVec, muRealObs, sigma, log = T))
+  
+  return(-(loglik1+loglik2))
+}
